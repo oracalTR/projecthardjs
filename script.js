2fab3e83913bdb55452775506a7140caf3dfb923
Pavel Trushnikov <oracal@mail.ru>
2021-11-30 00:27:26 +0300

    Выполнение третье усложненного урока

--- script.js
+++ script.js
@@ -2,6 +2,38 @@
 let num = 266219;
 let res = num.toString();
 let comp = 1;
+
+//1
+let lang = prompt('Введите язык докумена', 'Необходимо ввести ru или en');
+let weeks = {
+    ru: [ 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс', ],
+    en: [ 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su', ],
+};
+
+//a
+lang == 'ru' ? console.log(weeks.ru) : lang == 'en' ? console.log(weeks.en) : console.log('Язык документа не определен');
+
+//b
+switch(lang) {
+    case 'ru':
+        console.log(weeks.ru);
+        break;
+    case 'en':
+        console.log(weeks.en);
+        break;
+    default:    
+        console.log('Язык документа не определен');
+        break;
+}
+
+//c
+console.log(weeks[lang]);
+
+//2
+let namePerson = 'Павел';
+
+namePerson == 'Артём' ? console.log('Директор') : namePerson == 'Александр' ? console.log('Преподаватель') : console.log('Студент');
+
 for (let i = 0; i < res.length; i++) {
     comp *= res[i];
 }
